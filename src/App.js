5b960012e96fc54184b25e96c8adcb55f6275360
Chris De Leon <cdeleon.ca91@gmail.com>
2021-08-16 12:52:07 -0400

    setup app, cleaned files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { Form, Input, Button } from 'antd'
+import 'antd/dist/antd.css'
 
-function App() {
+export default function App() {
+  const handleSignup = ({email, password}) => {
+    fetch('https://auth-hash-api-cdl.web.app/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ email, password})
+  })
+  .then(response => response.json())
+  .then(data => alert(data.message))
+  .catch(err => alert(err))
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <Form
+    labelCol={{ span: 8}}
+    wrapperCol={{ span: 16}}
+    onFinish={handleSignup}
+    >
+      <h1>Sign Up</h1>
+<Form.Item
+label="Email"
+name="email"
+rules={[{ required: true, message: "Please input your email"}]}
+>
+  <Input/>
+</Form.Item>
+<Form.Item
+label="Password"
+name="password"
+rules={[{ required: true, message: "Please input your password"}]}
+>
+  <Input.Password />
+</Form.Item>
+<Form.Item wrapperCol={{ offset: 8, span:16 }}>
+  <Button type='primary' htmlType='submit'> Sign up
+  </Button>
+</Form.Item>
+    </Form>
+   )
+  }
+   
 
-export default App;
+
